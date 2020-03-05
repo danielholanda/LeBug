ac3108c5cc778af4043d1f63cd3ca5a4e5a73ba8
Daniel Holanda Noronha <danielhn@ece.ubc.ca>
2020-03-05 14:43:44 -0800

    Compiler almost working

--- emulator.py
+++ emulator.py
@@ -1,6 +1,7 @@
 import logging as log
 import sys, math
 import numpy as np
+from copy import deepcopy as copy
 
 # Setting Debug level (can be debug, info, warning, error and critical)
 log.basicConfig(stream=sys.stderr, level=log.DEBUG)
@@ -26,6 +27,12 @@ assert math.log(N, 2).is_integer(), "N must be a power of 2"
 assert math.log(M, 2).is_integer(), "N must be a power of 2" 
 assert M<=N, "M must be less or equal to N" 
 
+''' Useful functions '''
+class struct:
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+    def __repr__(self):
+        return str(self.__dict__)
 
 class CISC():
     # Input buffer class 
@@ -55,15 +62,14 @@ class CISC():
             self.input=np.zeros(N)
             self.output=np.zeros((M,N))
             self.vrf=np.zeros(FUVRF_SIZE)
-            self.addr=0
+            self.config=struct(addr=0)
 
         def step(self,input_value):
-
             # Check if the vector is within M ranges
-            log.debug('Filtering using the following ranges:'+str(self.vrf[self.addr:self.addr+M+1]))
+            log.debug('Filtering using the following ranges:'+str(self.vrf[self.config.addr:self.config.addr+M+1]))
             for i in range(M):
-                low_range = self.vrf[self.addr+i]
-                high_range = self.vrf[self.addr+i+1]
+                low_range = self.vrf[self.config.addr+i]
+                high_range = self.vrf[self.config.addr+i+1]
                 within_range = np.all([self.input>low_range, self.input<=high_range],axis=0)
                 self.output[i]=within_range[:]
 
@@ -76,15 +82,15 @@ class CISC():
         def __init__(self,N,M):
             self.input=np.zeros((M,N))
             self.output=np.zeros(N)
-            self.axis=0
+            self.config=struct(axis=0)
 
         def step(self,input_value):
             # Reduce matrix along a given axis
-            self.output=np.sum(self.input,axis=self.axis)
-            if self.axis==0:
-                log.debug('Reducing matrix along N axis (axis = '+str(self.axis)+')')
-            elif self.axis==1:
-                log.debug('Reducing matrix along M axis (axis = '+str(self.axis)+')')
+            self.output=np.sum(self.input,axis=self.config.axis)
+            if self.config.axis==0:
+                log.debug('Reducing matrix along N axis (axis = '+str(self.config.axis)+')')
+            elif self.config.axis==1:
+                log.debug('Reducing matrix along M axis (axis = '+str(self.config.axis)+')')
                 if N!=M:
                     log.debug('Padding results with '+str(N-M)+' zeros')
                     self.output=np.concatenate((self.output,np.zeros(N-M)))
@@ -97,13 +103,13 @@ class CISC():
         def __init__(self,N):
             self.input=np.zeros(N)
             self.output=np.zeros(N)
-            self.op=0
+            self.config=struct(reset=0,op=0)
 
         def step(self,input_value):
-            if self.reset:
+            if self.config.reset:
                 log.debug('Resetting vector-vector ALU output')
                 self.output = np.zeros(N)
-            if self.op==0:
+            if self.config.op==0:
                 log.debug('Adding using vector-vector ALU')
                 self.output = self.output + self.input
             self.input=input_value
@@ -122,6 +128,14 @@ class CISC():
         chain = self.mvru.step(chain)
         chain = self.vvalu.step(chain)
 
+    def run(self,compiled_firmware):
+        for instr in compiled_firmware:
+            # Dispatch Instructions to each functional unit
+            self.fu.config ,self.mvru.config, self.vvalu.config = instr
+            # Keep stepping through the circuit as long as we have instructions to execute
+            self.step()
+
+
 
 # Instantiate processor
 proc = CISC(N,M,IB_DEPTH,FUVRF_SIZE)
@@ -130,40 +144,43 @@ proc = CISC(N,M,IB_DEPTH,FUVRF_SIZE)
 proc.fu.vrf=list(range(FUVRF_SIZE)) # Initializing fuvrf
 
 # Hardware configurations (that can be done by VLIW instruction)
-proc.fu.addr=0
-proc.mvru.axis=1
-proc.vvalu.reset=0
-proc.vvalu.op=0
-
 class compiler():
     # ISA
     def begin_chain(self):
-        self.fu={"addr":0}
-        self.mvru={"axis":0}
-        self.vvalu={"reset":0,"op":0}
+        self.fu, self.mvru, self.vvalu = copy(self.pass_through)
     def filter(self,addr):
-        self.fu={"addr":addr}
+        self.fu.addr=addr
     def reduceN(self):
-        self.mvru={"axis":0}
+        self.mvru.axis=0
     def reduceM(self):
-        self.mvru={"axis":1}
+        self.mvru.axis=1
     def vv_add_new(self):
-        self.vvalu={"reset":1,"op":0}
+        self.vvalu.reset=1
+        self.vvalu.op=0
     def vv_add(self):
-        self.vvalu={"reset":0,"op":0}
+        self.vvalu.reset=0
+        self.vvalu.op=0
     def end_chain(self):
-        chain_instrs = {"fu":self.fu,"mvru":self.mvru,"vvalu":self.vvalu}
-        self.firmware.append([chain_instrs])
-    def compile():
-    	# We will compile to abstract away the idea of cycles
-    	CONTINUE HERE
-    	return compiled_firmware
+        self.firmware.append(copy([self.fu,self.mvru,self.vvalu]))
+    def compile(self):
+        # We will compile to abstract away the idea of cycles
+        # Input: List of operations each chain needs to perform
+        # Output: For each cycle, which operation each function unit should perform
+        compiled_firmware=[]
+        pipeline_depth=3
+        number_of_chains=len(self.firmware)
+        for i in range(pipeline_depth+number_of_chains):
+            chain_instrs = self.pass_through[:]
+            for j in range(pipeline_depth):
+                if i<number_of_chains and i>j-1:
+                    chain_instrs[j]=self.firmware[i][j]
+            compiled_firmware.append(chain_instrs)
+        return compiled_firmware
 
     def __init__(self):
         self.firmware = []
-        self.fu=None
-        self.mvru=None
-        self.vvalu=None
+        self.pass_through = [struct(addr=0),struct(axis=0),struct(reset=0,op=0)]
+        self.fu, self.mvru, self.vvalu = self.pass_through[:]
 
 # Firmware for a generic distribution
 def distribution(bins):
@@ -172,25 +189,23 @@ def distribution(bins):
     for i in range(bins/M):
         cp.begin_chain()
         cp.filter(i*M)
-        cp.reduceN()
+        cp.reduceM()
         cp.vv_add()
         cp.end_chain()
     return cp.compile()
 
 compiled_firmware = distribution(2*M)
 
-print(compiled_firmware)
+for chain in compiled_firmware:
+    print(chain)
+
 
 # Feed one value to input buffer
 input_vector = np.random.rand(N)*4
 proc.ib.push(input_vector)
 
-
 # Step through it until we get the result
-proc.step()
-proc.step()
-proc.step()
-proc.step()
+proc.run(compiled_firmware)
 print(proc.fu.output)
 print(proc.mvru.output)
 print(proc.vvalu.output)
