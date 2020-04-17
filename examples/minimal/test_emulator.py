8d37e2ebcc4ab706c77061577451803dfb7e4638
Daniel Holanda Noronha <holand.daniel@gmail.com>
2020-04-17 16:24:55 -0700

    firmware in separate file

--- examples/minimal/test_emulator.py
+++ examples/minimal/test_emulator.py
@@ -1,6 +1,7 @@
 import sys
-sys.path.insert(1, '../../src/emulator/')
-from emulator import *
+sys.path.insert(1, '../../src/')
+from emulator.emulator import *
+import firmware.firmware as firm
 import math, yaml
 
 # Read YAML configuration file and declare those as global variables
@@ -19,25 +20,13 @@ def testSimpleDistribution():
     # Initial hardware setup
     proc.fu.vrf=list(range(FUVRF_SIZE*M)) # Initializing fuvrf
 
-    # Firmware for a generic distribution
-    def distribution(bins):
-        assert bins%M==0, "Number of bins must be divisible by M for now"
-        cp = compiler(N,M)
-        for i in range(bins/M):
-            cp.begin_chain()
-            cp.vv_filter(i)
-            cp.m_reduce(axis='M')
-            cp.vv_add(i)
-            cp.v_commit(M)
-            cp.end_chain()
-        return cp.compile()
-
-    fw = distribution(2*M)
+    cp = compiler(N,M)
+    fw = firm.distribution(cp,bins=2*M,M=M)
 
     # Feed one value to input buffer
     np.random.seed(42)
     input_vector = np.random.rand(N)*8
-    proc.ib.push([input_vector,False])
+    proc.ib.push([input_vector,True])
 
     # Step through it until we get the result
     proc.config(fw)
@@ -55,21 +44,8 @@ def testDualDistribution():
     # Initial hardware setup
     proc.fu.vrf=list(range(FUVRF_SIZE*M)) # Initializing fuvrf
 
-    # Firmware for a distribution with 2 sets of N values
-    def distribution(bins):
-        assert bins%M==0, "Number of bins must be divisible by M for now"
-        cp = compiler(N,M)
-        for i in range(bins/M):
-            cp.begin_chain()
-            cp.vv_filter(i)
-            cp.m_reduce('M')
-            cp.vv_add(i,'notfirst')
-            cp.v_cache(i)
-            cp.v_commit(M,'last')
-            cp.end_chain()
-        return cp.compile()
-
-    fw = distribution(2*M)
+    cp = compiler(N,M)
+    fw = firm.distribution(cp,bins=2*M,M=M)
 
     # Feed one value to input buffer
     np.random.seed(42)
@@ -94,31 +70,7 @@ def testSummaryStats():
     cp = compiler(N,M)
 
     proc.fu.vrf=list(np.concatenate(([0.,float('inf')],list(reversed(range(FUVRF_SIZE*M-2)))))) # Initializing fuvrf for sparsity
-
-
-    # Firmware for a distribution with 2 sets of N values
-    def summaryStats():
-        
-        # Sum of all values
-        cp.begin_chain()
-        cp.vv_add(0,'notfirst')
-        cp.v_cache(0)
-        cp.v_reduce()
-        cp.v_commit(1,'last')
-        cp.end_chain()
-
-        # Number of sparse elements
-        cp.begin_chain()
-        cp.vv_filter(0)
-        cp.m_reduce('N')
-        cp.vv_add(1,'notfirst')
-        cp.v_cache(1)
-        cp.v_reduce()
-        cp.v_commit(1,'last')
-        cp.end_chain()
-        return cp.compile()
-
-    fw = summaryStats()
+    fw = firm.summaryStats(cp)
 
     # Feed one value to input buffer
     np.random.seed(0)
@@ -149,22 +101,8 @@ def testSpatialSparsity():
     # Instantiate processor
     proc = CISC(N,M,IB_DEPTH,FUVRF_SIZE,VVVRF_SIZE,TB_SIZE)
     cp = compiler(N,M)
-
     proc.fu.vrf=list(np.concatenate(([0.,float('inf')],list(reversed(range(FUVRF_SIZE*M-2)))))) # Initializing fuvrf for sparsity
-
-
-    # Firmware for a distribution with 2 sets of N values
-    def summaryStats():
-
-        # Spatial sparsity
-        cp.begin_chain()
-        cp.vv_filter(0)
-        cp.m_reduce('N')
-        cp.v_commit(N)
-        cp.end_chain()
-        return cp.compile()
-
-    fw = summaryStats()
+    fw = firm.spatialSparsity(cp,N)
 
     # Feed one value to input buffer
     np.random.seed(0)
@@ -189,33 +127,7 @@ def testCorrelation():
     # Instantiate processor
     proc = CISC(N,M,IB_DEPTH,FUVRF_SIZE,VVVRF_SIZE,TB_SIZE)
     cp = compiler(N,M)
-
-    # Firmware for a distribution with 2 sets of N values
-    def correlation():
-
-        # sum(X*Y) [Assuming that Y is stored in addr0]
-        cp.begin_chain()
-        cp.vv_mul(0)
-        cp.v_reduce()
-        cp.v_commit(1)
-        cp.end_chain()
-
-        # sum(X) [Storing X in addr0, which will become the Y of next vector]
-        cp.begin_chain()
-        cp.v_cache(0)
-        cp.v_reduce()
-        cp.v_commit(1)
-        cp.end_chain()
-
-        # sum(X*X)
-        cp.begin_chain()
-        cp.vv_mul(0)
-        cp.v_reduce()
-        cp.v_commit(1)
-        cp.end_chain()
-        return cp.compile()
-
-    fw = correlation()
+    fw = firm.correlation(cp)
 
     # Feed one value to input buffer
     np.random.seed(0)
@@ -234,7 +146,6 @@ def testCorrelation():
 
     v = proc.dp.v_out
     x, y, xx, yy, xy = v[1], v[4], v[2], v[5], v[3]
-    #x, y, xx, yy, xy = np.sum(input_vector1), np.sum(input_vector2), np.sum(input_vector1*input_vector1), np.sum(input_vector2*input_vector2), np.sum(input_vector1*input_vector2)
 
     # Note that the equation in this website is wrong, but the math is correct
     # https://www.investopedia.com/terms/c/correlation.asp
@@ -250,24 +161,7 @@ def testVectorChange():
     # Instantiate processor
     proc = CISC(N,M,IB_DEPTH,FUVRF_SIZE,VVVRF_SIZE,TB_SIZE)
     cp = compiler(N,M)
-
-    # Firmware for a distribution with 2 sets of N values
-    def vectorChange():
-
-        # Commit difference between current and previous sample
-        cp.begin_chain()
-        cp.vv_sub(0)
-        cp.v_reduce()
-        cp.v_commit(1)
-        cp.end_chain()
-
-        # Save vector to mem0
-        cp.begin_chain()
-        cp.v_cache(0)
-        cp.end_chain()
-        return cp.compile()
-
-    fw = vectorChange()
+    fw = firm.vectorChange(cp)
 
     # Feed one value to input buffer
     np.random.seed(0)
@@ -287,20 +181,3 @@ def testVectorChange():
     print("Passed test #6")
 
 testVectorChange()
-
-# Ideas for new instruments:
-# 1- Check elements that are between -inf and min_range OR NaN OR between max_range and Inf (given a specific activation)
-#   In the previous instrumentation, I would need one instrument for each of them (3x more memory)
-# 2- How often is the entire vector changing? (one bit per sample) [Would be more useful at each cyle]
-#		- Currently doing simple subtraction, but we could have a abs block after the subtraction
-#       - If we could send the value of the subtraction back up we could use the filter block to filer positive elements
-# 3- Correlation in a tricky way
-#       Done, but need to show steve how I did it
-#       Take away 
-#           - Calculating the correlation is quite complex
-#           - We can calulate the correlation using either 2 matrices of size (N) or 5 values
-#           - 2 of those values can be cached to the next cycle
-#           -> Instead of computing everything and ending up with one value, I ended up with 3 values
-#              -> The rest of the math is done offline  
-# 4 - Parity bit
-#       Same as Sum of all values, but only submit 1 bit instead of 1 value
---
+++ src/firmware/firmware.py
@@ -0,0 +1,105 @@
+# This files contains some of the different firmware that can be used by the HW and emulator
+
+# Firmware for a distribution with multiple sets of N values
+def distribution(cp,bins,M):
+    assert bins%M==0, "Number of bins must be divisible by M for now"
+    for i in range(bins/M):
+        cp.begin_chain()
+        cp.vv_filter(i)
+        cp.m_reduce('M')
+        cp.vv_add(i,'notfirst')
+        cp.v_cache(i)
+        cp.v_commit(M,'last')
+        cp.end_chain()
+    return cp.compile()
+
+# Summary statistics - Number of non-sparse elements
+def summaryStats(cp):
+    # Remember to properly initialize fu.vrf
+
+    # Sum of all values
+    cp.begin_chain()
+    cp.vv_add(0,'notfirst')
+    cp.v_cache(0)
+    cp.v_reduce()
+    cp.v_commit(1,'last')
+    cp.end_chain()
+
+    # Number of sparse elements
+    cp.begin_chain()
+    cp.vv_filter(0)
+    cp.m_reduce('N')
+    cp.vv_add(1,'notfirst')
+    cp.v_cache(1)
+    cp.v_reduce()
+    cp.v_commit(1,'last')
+    cp.end_chain()
+    return cp.compile()
+
+# Calculate spatial sparsity
+def spatialSparsity(cp,N):
+    # Remember to properly initialize fu.vrf
+    cp.begin_chain()
+    cp.vv_filter(0)
+    cp.m_reduce('N')
+    cp.v_commit(N)
+    cp.end_chain()
+    return cp.compile()
+
+# Check if previous vector changed
+def vectorChange(cp):
+
+    # Commit difference between current and previous sample
+    cp.begin_chain()
+    cp.vv_sub(0)
+    cp.v_reduce()
+    cp.v_commit(1)
+    cp.end_chain()
+
+    # Save vector to mem0
+    cp.begin_chain()
+    cp.v_cache(0)
+    cp.end_chain()
+    return cp.compile()
+
+# Self correlation with the previous sample
+def correlation(cp):
+
+    # sum(X*Y) [Assuming that Y is stored in addr0]
+    cp.begin_chain()
+    cp.vv_mul(0)
+    cp.v_reduce()
+    cp.v_commit(1)
+    cp.end_chain()
+
+    # sum(X) [Storing X in addr0, which will become the Y of next vector]
+    cp.begin_chain()
+    cp.v_cache(0)
+    cp.v_reduce()
+    cp.v_commit(1)
+    cp.end_chain()
+
+    # sum(X*X)
+    cp.begin_chain()
+    cp.vv_mul(0)
+    cp.v_reduce()
+    cp.v_commit(1)
+    cp.end_chain()
+    return cp.compile()
+
+# Ideas for new instruments:
+# 1- Check elements that are between -inf and min_range OR NaN OR between max_range and Inf (given a specific activation)
+#   In the previous instrumentation, I would need one instrument for each of them (3x more memory)
+# 2- How often is the entire vector changing? (one bit per sample) [Would be more useful at each cyle]
+#       - Currently doing simple subtraction, but we could have a abs block after the subtraction
+#       - If we could send the value of the subtraction back up we could use the filter block to filer positive elements
+# 3- Correlation in a tricky way
+#       Done, but need to show steve how I did it
+#       Take away 
+#           - Calculating the correlation is quite complex
+#           - We can calulate the correlation using either 2 matrices of size (N) or 5 values
+#           - 2 of those values can be cached to the next cycle
+#           -> Instead of computing everything and ending up with one value, I ended up with 3 values
+#              -> The rest of the math is done offline  
+# 4 - Parity bit
+#       Same as Sum of all values, but only submit 1 bit instead of 1 value
