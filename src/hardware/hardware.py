b9b5f57901f1e4bea16dbc91f0ab28659b1dd7c5
Daniel Holanda Noronha <holand.daniel@gmail.com>
2020-05-04 16:41:41 -0700

    Using recurrent module definitions

--- src/hardware/hardware.py
+++ src/hardware/hardware.py
@@ -19,22 +19,43 @@ class struct:
 class rtlHw():
     
     # This is a python class that holds RTL code
-    class rtlFile():
+    class rtlModule():
 
         # All include files go here
         def include(self,file):
             self.includes.append(file)
+
+        # Recursively adds submodules to module
+        def addSubmodule(self,sm_name,inputLogic=[],outputLogic=[]):
+            self.sm.__dict__[sm_name]=self.parent.rtlModule(self,inputLogic,outputLogic)
 
         # Dump RTL class into readable RTL
         def dump(self):
             rtl=[]
             for i in self.includes:
                 rtl.append('`include "'+i+'"')
+
+            # Continue here
+            for i in self.inputLogic:
+                rtl.append('still need to add input logic '+i[0])
+
+            for i in self.outputLogic:
+                rtl.append('still need to add outputLogic logic '+i[0])
+
+            # Do recursive dumps for subclasses
+            for m in self.sm.__dict__.keys():
+                rtl=rtl+self.sm.__dict__[m].dump()
             return rtl
 
         # Initializes the RTL file class
-        def __init__(self):
-            self.includes=[]
+        def __init__(self,parent,inputLogic=[],outputLogic=[]):
+            self.parent = parent
+            self.includes=[]            # Stores include files
+            self.wires=[]               # Stores wires
+            self.regs=[]                # Stores regs
+            self.inputLogic=inputLogic  # Store all input logic ports
+            self.outputLogic=outputLogic# Store all output logic ports
+            self.sm=struct()            # All submodules go here
 
     def generateRtl(self):
 
@@ -44,12 +65,19 @@ class rtlHw():
             shutil.rmtree(rtl_folder)
         shutil.copytree(self.hwFolder+"/buildingBlocks", rtl_folder)
 
+        # Create RTL using custom RTL class
+        rtl = self.rtlModule(self)
+
         # Includes all needed files
-        self.rtl.include("inputBuffer.sv")
+        rtl.include("inputBuffer.sv")
+
+        il=[['clk',1],['vector',self.N]]        
+        rtl.addSubmodule("main",inputLogic=il)
 
         # Writes to file
         f = open(rtl_folder+"/debugProcessor.sv", "w")
-        for l in self.rtl.dump():
+        print(rtl.dump())
+        for l in rtl.dump():
             f.write(l+"\n")
         f.close()
 
@@ -59,7 +87,8 @@ class rtlHw():
         assert math.log(M, 2).is_integer(), "N must be a power of 2" 
         assert M<=N, "M must be less or equal to N" 
 
+        self.N=N
+        self.M=M
         self.hwFolder = os.path.dirname(os.path.realpath(__file__))
-        self.rtl = self.rtlFile()
-
         self.generateRtl()
+        
