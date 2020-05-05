a8bf8a41a1abc10a0eff29283744ac5b4a4f3803
Daniel Holanda Noronha <holand.daniel@gmail.com>
2020-05-05 09:12:52 -0700

    Dump improvements

--- src/hardware/hardware.py
+++ src/hardware/hardware.py
@@ -26,30 +26,39 @@ class rtlHw():
             self.includes.append(file)
 
         # Recursively adds submodules to module
-        def addSubmodule(self,sm_name,inputLogic=[],outputLogic=[]):
-            self.sm.__dict__[sm_name]=self.parent.rtlModule(self,inputLogic,outputLogic)
+        def declareSubmodule(self,sm_name,inputLogic=[],outputLogic=[]):
+            self.sm.__dict__[sm_name]=self.parent.rtlModule(self,sm_name,inputLogic,outputLogic)
 
         # Dump RTL class into readable RTL
         def dump(self):
             rtl=[]
+
+            # Add includes
             for i in self.includes:
                 rtl.append('`include "'+i+'"')
 
-            # Continue here
-            for i in self.inputLogic:
-                rtl.append('still need to add input logic '+i[0])
+            # Instantiate module if inputs are available
+            if self.inputLogic+self.outputLogic!=[]:
+                rtl.append('module  mux_using_assign (')
+                # Add inputs
+                for i in self.inputLogic:
+                    rtl.append('still need to add input logic '+i[0])
 
-            for i in self.outputLogic:
-                rtl.append('still need to add outputLogic logic '+i[0])
+                for i in self.outputLogic:
+                    rtl.append('still need to add outputLogic logic '+i[0])
 
-            # Do recursive dumps for subclasses
-            for m in self.sm.__dict__.keys():
-                rtl=rtl+self.sm.__dict__[m].dump()
-            return rtl
+                # Do recursive dumps for subclasses
+                for m in self.sm.__dict__.keys():
+                    rtl=rtl+self.sm.__dict__[m].dump()
+            else:
+                print(self.name+" has no inputs/outputs")
+
+            return rtl 
 
         # Initializes the RTL file class
-        def __init__(self,parent,inputLogic=[],outputLogic=[]):
-            self.parent = parent
+        def __init__(self,parent,name,inputLogic=[],outputLogic=[]):
+            self.name=name
+            self.parent = parent        # name of submodule
             self.includes=[]            # Stores include files
             self.wires=[]               # Stores wires
             self.regs=[]                # Stores regs
@@ -66,17 +75,21 @@ class rtlHw():
         shutil.copytree(self.hwFolder+"/buildingBlocks", rtl_folder)
 
         # Create RTL using custom RTL class
-        rtl = self.rtlModule(self)
+        inputLogic=[['clk',1],['valid',1],['vector',self.N]] 
+        rtl = self.rtlModule(self,"main",inputLogic)
 
         # Includes all needed files
         rtl.include("inputBuffer.sv")
 
-        il=[['clk',1],['vector',self.N]]        
-        rtl.addSubmodule("main",inputLogic=il)
+        # Tells the class about the included modules
+        #rtl.includedSubmodule("inputBuffer")
+
+        inputLogic=[['clk',1],['vector',self.N]]        
+        rtl.declareSubmodule("submodule1",inputLogic)
+        rtl.declareSubmodule("submodule2")
 
         # Writes to file
         f = open(rtl_folder+"/debugProcessor.sv", "w")
-        print(rtl.dump())
         for l in rtl.dump():
             f.write(l+"\n")
         f.close()
