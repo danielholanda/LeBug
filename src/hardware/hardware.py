24b79d86b9e061e36d43ab902c243678bc5acf50
Daniel Holanda Noronha <holand.daniel@gmail.com>
2020-10-16 11:30:38 -0700

    Started to create hw compiler

--- src/hardware/hardware.py
+++ src/hardware/hardware.py
@@ -31,6 +31,72 @@ def run(cmd,wait=True):
 
 class rtlHw():
 
+    # Hardware configurations (that can be done by VLIW instruction)
+    class compiler():
+        # ISA
+        def begin_chain(self):
+            self.fu, self.mvru, self.vsru, self.vvalu, self.dp = copy(self.pass_through)
+        def vv_filter(self,addr):
+            self.fu.filter=1
+            self.fu.addr=addr
+        def m_reduce(self,axis='N'):
+            if axis=='N':
+                self.mvru.axis=1
+            elif axis=='M':
+                self.mvru.axis=2
+            else:
+                assert False, "Unknown axis for instruction m_reduce"
+        def v_reduce(self):
+            self.vsru.op=1
+        def vv_add(self,addr,condition=None):
+            self.vvalu.op=1
+            self.vvalu.addr=addr
+            if condition=="last" or condition=="notlast" or condition=="first" or condition=="notfirst" or condition is None:
+                self.vvalu.cond[condition]=True
+            else:
+                assert False, "Condition not understood"
+        def vv_mul(self,addr,condition=None):
+            self.vvalu.op=2
+            self.vvalu.addr=addr
+            if condition=="last" or condition=="notlast" or condition=="first" or condition=="notfirst" or condition is None:
+                self.vvalu.cond[condition]=True
+            else:
+                assert False, "Condition not understood"
+        def vv_sub(self,addr,condition=None):
+            self.vvalu.op=3
+            self.vvalu.addr=addr
+            if condition=="last" or condition=="notlast" or condition=="first" or condition=="notfirst" or condition is None:
+                self.vvalu.cond[condition]=True
+            else:
+                assert False, "Condition not understood"
+        def v_cache(self,cache_addr):
+            self.vvalu.cache=1
+            self.vvalu.cache_addr=cache_addr
+        def v_commit(self,size=None,condition=None):
+            if size is None:
+                size = self.N
+            self.dp.commit=1
+            if size==self.N or size==self.M or size==1:
+                self.dp.size=size
+            else:
+                assert False, "Cannot commit "+str(size)+" elements"
+            if condition=="last" or condition=="notlast" or condition=="first" or condition=="notfirst" or condition is None:
+                self.dp.cond[condition]=True
+            else:
+                assert False, "Condition not understood"
+        def end_chain(self):
+            self.firmware.append(copy([self.fu,self.mvru,self.vsru,self.vvalu,self.dp]))
+        def compile(self):
+            return self.firmware
+        def __init__(self,N,M,MAX_CHAINS):
+            self.N = N
+            self.M = M
+            self.MAX_CHAINS = MAX_CHAINS
+            self.firmware = []
+            no_cond={'last':False,'notlast':False,'first':False,'notfirst':False}
+            self.pass_through = [struct(filter=0,addr=0),struct(axis=0),struct(op=0),struct(op=0,addr=0,cond=copy(no_cond),cache=0,cache_addr=0),struct(commit=0,size=0,cond=copy(no_cond))]
+            self.fu, self.mvru, self.vsru, self.vvalu, self.dp = self.pass_through[:]
+
     # This class describes an instantiated rtlModule
     class rtlInstance():
 
@@ -567,6 +633,6 @@ class rtlHw():
         self.testbench_inputs=[]    # Stores inputs to testbench
         self.steps=0 # Number of steps for testbench
         self.top=None # used to store all rtl info later on
-
         self.tb_var_names = None
+        self.compiler = self.compiler(M,N,MAX_CHAINS)
         
