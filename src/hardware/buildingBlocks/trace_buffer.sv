ed52b85a2b98d68a875d13c8423888ac4d9b466f
Daniel Holanda Noronha <holand.daniel@gmail.com>
2020-10-19 11:56:11 -0700

    Started to create trace buffer

---
+++ src/hardware/buildingBlocks/trace_buffer.sv
@@ -0,0 +1,91 @@
+ //-----------------------------------------------------
+ // Design Name : Trace Buffer
+ // Function    : Circular trace buffer of depth TB_SIZE
+ //-----------------------------------------------------
+`include "ram_dual_port.sv"
+
+ module  inputBuffer #(
+  parameter N=8,
+  parameter DATA_WIDTH=32,
+  parameter TB_SIZE=64
+  )
+  (
+  input logic clk,
+  input logic tracing,
+  input logic valid_in,
+  input logic [DATA_WIDTH-1:0] vector_in [N-1:0]
+ );
+
+    //----------Internal Variables------------
+    wire empty,full;
+
+    parameter LATENCY = 2;
+    parameter RAM_LATENCY = LATENCY-1;
+    parameter MEM_WIDTH = N*DATA_WIDTH;
+
+    //-------------Code Start-----------------
+
+    // Instantiate memory to implement queue
+    reg [$clog2(IB_DEPTH)-1:0] mem_address_a=1;
+    reg [$clog2(IB_DEPTH)-1:0] mem_address_b=0;
+    wire mem_write_enable_a;
+    reg mem_write_enable_b=0;
+    wire [MEM_WIDTH-1:0] mem_in_a;
+    reg [MEM_WIDTH-1:0] mem_in_b=0;
+    wire [MEM_WIDTH-1:0] mem_out_a;
+    wire [MEM_WIDTH-1:0] mem_out_b;
+    ram_dual_port mem (
+      .clk( clk ),
+      .clken( 1'b1 ),
+      .address_a( mem_address_a ),
+      .address_b( mem_address_b ),
+      .wren_a( mem_write_enable_a ),
+      .wren_b( mem_write_enable_b ),
+      .data_a( mem_in_a ),
+      .data_b( mem_in_b ),
+      .byteena_a( 1'b1 ),
+      .byteena_b( 1'b1 ),
+      .q_a( mem_out_a ),
+      .q_b( mem_out_b)
+    );
+    defparam mem.width_a = MEM_WIDTH;
+    defparam mem.width_b = MEM_WIDTH;
+    defparam mem.widthad_a = $clog2(IB_DEPTH);
+    defparam mem.widthad_b = $clog2(IB_DEPTH);
+    defparam mem.width_be_a = 1;
+    defparam mem.width_be_b = 1;
+    defparam mem.numwords_a = IB_DEPTH;
+    defparam mem.numwords_b = IB_DEPTH;
+    defparam mem.latency = RAM_LATENCY;
+    defparam mem.init_file = "traceBuffer.mif";
+
+    //CONTINUE HEEEERREEEE
+    always @(posedge clk) begin
+
+        // Logic for enqueuing values
+        if (enqueue==1'b1 & full==1'b0) begin
+            mem_address_a <= mem_address_a<IB_DEPTH-1 ? mem_address_a+1'b1 : 0;
+        end
+
+        //Logic for dequeuing
+        if (dequeue==1'b1 & empty==1'b0) begin
+            mem_address_b <= mem_address_b<IB_DEPTH-1 ? mem_address_b+1'b1 : 0;
+        end
+
+    end
+
+
+    // Directly assign module inputs to port A of memory
+    assign mem_in_a = { >> { vector_in }};
+    assign mem_write_enable_a = enqueue;
+
+    // Module output is the output of the queue
+    assign vector_out = { >> { mem_out_b }};
+
+    // Check if queue is empty/full
+    assign empty = (mem_address_a-mem_address_b==0) | (mem_address_a==0 & mem_address_b==IB_DEPTH-1);
+    assign full = (mem_address_a==mem_address_b);
+
+    assign chainId_out=1'b0;
+ 
+ endmodule 
