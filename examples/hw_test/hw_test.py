169cebc80094a15d8f2cdcda825cf10b365475b2
Daniel Holanda Noronha <holand.daniel@gmail.com>
2021-01-06 16:00:25 -0800

    Changed hw tests

--- examples/hw_test/hw_test.py
+++ examples/hw_test/hw_test.py
@@ -18,6 +18,62 @@ def toInt(lst):
 
 print("BUG: STRATIX10 DUAL_PORT_RAM IS WORKING DIFFERENTLY FROM CYCLONE V DUAL_PORT_RAM")
 
+def raw():
+
+    # Overwrite YAML file to define how components are attached to eachother
+    BUILDING_BLOCKS=['InputBuffer', 'FilterReduceUnit','VectorVectorALU','VectorScalarReduce','DataPacker','TraceBuffer']
+
+    # Instantiate HW and Emulator Processors
+    DATA_WIDTH=32
+    MAX_CHAINS=4
+    IB_DEPTH=32
+    TB_SIZE=8
+    hw_proc  = rtlHw(N,M,IB_DEPTH,FUVRF_SIZE,VVVRF_SIZE,TB_SIZE,DATA_WIDTH,MAX_CHAINS)
+    emu_proc = emulatedHw(N,M,IB_DEPTH,FUVRF_SIZE,VVVRF_SIZE,TB_SIZE,MAX_CHAINS,BUILDING_BLOCKS)
+
+    # Create common input values
+    np.random.seed(0)
+    input_vectors=[]
+    num_input_vectors=1
+    print("********** Input vectors **********")
+    for i in range(num_input_vectors):
+        input_vectors.append(np.random.randint(5, size=N))
+        hw_proc.push([input_vectors[i],False])
+        emu_proc.push([input_vectors[i],False])
+        print(f'Cycle {i}:\t{input_vectors[i]}')
+
+    # Initialize the memories the same way
+    emu_proc.fu.vrf=list(range(FUVRF_SIZE*M)) # Initializing fuvrf
+    hw_proc.top.mod.filterReduceUnit.mem['furf']['init_values']=[list(range(FUVRF_SIZE))]*M
+
+    # Configure firmware - Both HW and Emulator work with the same firmware
+    fw = firm.raw(hw_proc.compiler)
+    emu_proc.config(fw)
+    hw_proc.config(fw)
+
+    # Run HW simulation and emulation
+    steps=30
+    hw_results = hw_proc.run(steps=steps,gui=False,log=False)
+    emu_results = emu_proc.run(steps=steps)
+
+    # Filter Results
+    emu_trace_buffer = emu_results['tb'][-1];
+    hw_trace_buffer = np.array(toInt(hw_results['tb']['mem_data']))
+    #hw_trace_buffer = np.array(hw_results['tb']['mem_data'])
+
+    # Print Results
+    print("\n\n********** Emulation results **********")
+    print(emu_trace_buffer)
+    print("\n********** Hardware results **********")
+    print(hw_trace_buffer)
+
+    # Verify that results are equal
+    assert np.allclose(emu_trace_buffer,hw_trace_buffer)
+    print("Passed test #1")
+
+raw()
+
+
 def multipleChains():
 
     # Overwrite YAML file to define how components are attached to eachother
@@ -68,8 +124,8 @@ def multipleChains():
     print(hw_trace_buffer)
 
     # Verify that results are equal
-    assert np.allclose(emu_trace_buffer,hw_trace_buffer), "Failed to match emulator and hardware in FRU test"
-    print("Passed test #1")
+    assert np.allclose(emu_trace_buffer,hw_trace_buffer)
+    print("Passed test #2")
 
 multipleChains()
 
@@ -124,8 +180,8 @@ def correlation():
     print(hw_trace_buffer)
 
     # Verify that results are equal
-    assert np.allclose(emu_trace_buffer,hw_trace_buffer), "Failed to match emulator and hardware in FRU test"
-    print("Passed test #2")
+    assert np.allclose(emu_trace_buffer,hw_trace_buffer)
+    print("Passed test #3")
 
 correlation()
 
@@ -185,8 +241,8 @@ def conditions():
     print(hw_trace_buffer)
 
     # Verify that results are equal
-    assert np.allclose(emu_trace_buffer,hw_trace_buffer), "Failed to match emulator and hardware in FRU test"
-    print("Passed test #3")
+    assert np.allclose(emu_trace_buffer,hw_trace_buffer)
+    print("Passed test #4")
 
 conditions()
 
@@ -247,8 +303,8 @@ def distribution():
     print(hw_trace_buffer)
 
     # Verify that results are equal
-    assert np.allclose(emu_trace_buffer,hw_trace_buffer), "Failed to match emulator and hardware in FRU test"
-    print("Passed test #4")
+    assert np.allclose(emu_trace_buffer,hw_trace_buffer)
+    print("Passed test #5")
 
 distribution()
 
